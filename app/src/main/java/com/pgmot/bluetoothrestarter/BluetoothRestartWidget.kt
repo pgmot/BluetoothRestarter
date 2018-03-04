f9c5867ab9d7a4fc4b20e54dcfe99b7cd7206be6
mot <mot.contact.address.pc@gmail.com>
2018-03-04 13:09:47 +0900

    できた

--- app/src/main/java/com/pgmot/bluetoothrestarter/BluetoothRestartWidget.kt
+++ app/src/main/java/com/pgmot/bluetoothrestarter/BluetoothRestartWidget.kt
@@ -1,9 +1,14 @@
 package com.pgmot.bluetoothrestarter
 
+import android.app.ActivityManager
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
+import android.util.Log
 import android.widget.RemoteViews
+
 
 /**
  * Implementation of App Widget functionality.
@@ -17,20 +22,29 @@ class BluetoothRestartWidget : AppWidgetProvider() {
         }
     }
 
-    override fun onEnabled(context: Context) {
-        // Enter relevant functionality for when the first widget is created
+    override fun onReceive(context: Context?, intent: Intent?) {
+        super.onReceive(context, intent)
+
+        when (intent?.action) {
+            "com.pgmot.bluetoothrestarter.action.RESTART" -> restartBluetooth(context)
+        }
     }
 
-    override fun onDisabled(context: Context) {
-        // Enter relevant functionality for when the last widget is disabled
+    private fun restartBluetooth(context: Context?) {
+        val activityManager = context?.getSystemService(android.content.Context.ACTIVITY_SERVICE) as ActivityManager
+        activityManager.killBackgroundProcesses("com.android.bluetooth")
     }
 
     companion object {
         internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
+            val intent = Intent("com.pgmot.bluetoothrestarter.action.RESTART")
+            val pendingIntent = PendingIntent.getBroadcast(context, appWidgetId, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
             // Construct the RemoteViews object
             val views = RemoteViews(context.packageName, R.layout.bluetooth_restart_widget)
 
             views.setImageViewResource(R.id.restart, android.R.drawable.ic_menu_rotate)
+            views.setOnClickPendingIntent(R.id.restart, pendingIntent)
 
             // Instruct the widget manager to update the widget
             appWidgetManager.updateAppWidget(appWidgetId, views)
